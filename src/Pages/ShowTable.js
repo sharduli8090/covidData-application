78c71517902a5e8a0f9e1b8f2e793a7b76bbd5b6
sharduli8090 <shardulipandey8090@gmail.com>
2022-12-18 17:51:37 +0530

    Made the Table Page

--- src/Pages/ShowTable.js
+++ src/Pages/ShowTable.js
@@ -1,11 +1,71 @@
-import React from "react";
-import Table from '../Components/Table/Table'
+import React, {useState, useEffect} from "react";
 
-export default function ShowTable(){
-    return(
-        <>
-        I am ShowTable!<br/>
-        <Table/>
+import '../assets/styles/showTable.css';
+
+export default function ShowTable() {
+
+
+    const [countryRegionWise, setcountryRegionWise] = useState([]);
+
+    const totalData = () => {
+
+
+        const options1 = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '060a0e62b3msh3b4dceebd52177fp1e5142jsn9222f9a719e6',
+                'X-RapidAPI-Host': 'covid-19-coronavirus-statistics.p.rapidapi.com'
+            }
+        };
+        fetch('https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=kl', options1).then(response => response.json()).then(response => {
+            console.log(response)
+            console.log(response.data.covid19Stats);
+            setcountryRegionWise(response.data.covid19Stats)
+        }).catch(err => console.error(err));
+
+    }
+
+
+    return (
+        <> {
+            totalData()
+        }
+            <div className="main-table-box">
+                <table>
+                    <tr>
+                        <th id="header-table" colSpan='4'>World Data</th>
+                    </tr>
+                    <tr>
+                        <th>Place</th>
+                        <th>Confirmed Cases</th>
+                        <th>Deaths Reported</th>
+                        <th>Last Updated</th>
+                    </tr>
+                    {
+                    countryRegionWise.map((e) => {
+                        return (
+                            <>
+                                <tr>
+
+                                    <td>{
+                                        e.keyId
+                                    }</td>
+                                    <td>{
+                                        e.confirmed
+                                    }</td>
+                                    <td>{
+                                        e.deaths
+                                    }</td>
+                                    <td>{
+                                        e.lastUpdate
+                                    }</td>
+                                </tr>
+                            </>
+                        )
+
+                    })
+                } </table>
+            </div>
         </>
     );
 }
